131f7ce57bb8b49da578628bf8cf812efe78d391
ivcylc <118931037+ivcylc@users.noreply.github.com>
2025-07-21 18:49:38 +0800

    Update hide_prompt.py

--- peft/prompt/hide_prompt.py
+++ peft/prompt/hide_prompt.py
@@ -131,8 +131,11 @@ class EPrompt(nn.Module):
                         batched_prompt_raw = self.prompt_copy[idx]
                     else:
                         batched_prompt_raw = self.prompt[idx]
-                batched_prompt_raw = batched_prompt_raw.unsqueeze(1) 
-                batch_size, top_k, dual, num_layers, length, embed_dim = batched_prompt_raw.shape
+                try:
+                    batch_size, top_k, dual, num_layers, length, embed_dim = batched_prompt_raw.shape
+                except:
+                    batched_prompt_raw = batched_prompt_raw.unsqueeze(1) 
+                    batch_size, top_k, dual, num_layers, length, embed_dim = batched_prompt_raw.shape
                 batched_prompt = batched_prompt_raw.reshape(
                     batch_size, top_k, dual, num_layers, length, self.num_heads, embed_dim // self.num_heads
                 )
